f3143f1bc35f219a584ac389261b32c510520a22
yogsingh10 <yogsingh10@WKWZTpd4ouAdzEn.global.publicisgroupe.net>
2022-10-14 09:02:44 +0530

    Code changes applay

--- src/main/java/com/ashokit/entity/ContactDtlsEntity.java
+++ src/main/java/com/ashokit/entity/ContactDtlsEntity.java
@@ -1,15 +1,13 @@
 package com.ashokit.entity;
 
+import java.util.Objects;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
-
-import lombok.Data;
-
 @Entity
-@Data
 @Table(name="contact_dtls")
 public class ContactDtlsEntity {
 	@Id
@@ -22,4 +20,50 @@ private String contactName;
 private String contactEmail;
 	@Column(name="contact_num")
 	private Long contactNumber;
+	public Integer getContactId() {
+		return contactId;
+	}
+	public void setContactId(Integer contactId) {
+		this.contactId = contactId;
+	}
+	public String getContactName() {
+		return contactName;
+	}
+	public void setContactName(String contactName) {
+		this.contactName = contactName;
+	}
+	public String getContactEmail() {
+		return contactEmail;
+	}
+	public void setContactEmail(String contactEmail) {
+		this.contactEmail = contactEmail;
+	}
+	public Long getContactNumber() {
+		return contactNumber;
+	}
+	public void setContactNumber(Long contactNumber) {
+		this.contactNumber = contactNumber;
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(contactEmail, contactId, contactName, contactNumber);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ContactDtlsEntity other = (ContactDtlsEntity) obj;
+		return Objects.equals(contactEmail, other.contactEmail) && Objects.equals(contactId, other.contactId)
+				&& Objects.equals(contactName, other.contactName) && Objects.equals(contactNumber, other.contactNumber);
+	}
+	@Override
+	public String toString() {
+		return "ContactDtlsEntity [contactId=" + contactId + ", contactName=" + contactName + ", contactEmail="
+				+ contactEmail + ", contactNumber=" + contactNumber + "]";
+	}
+	
 }
--- src/main/java/com/ashokit/pojos/ContactDTO.java
+++ src/main/java/com/ashokit/pojos/ContactDTO.java
@@ -1,12 +1,56 @@
 package com.ashokit.pojos;
 
-import lombok.Data;
-
-@Data
+import java.util.Objects;
 public class ContactDTO {
 	private Integer contactId;
 	private String contactName;
 	private String contactEmail;
 	private Long contactNumber;
+	public Integer getContactId() {
+		return contactId;
+	}
+	public void setContactId(Integer contactId) {
+		this.contactId = contactId;
+	}
+	public String getContactName() {
+		return contactName;
+	}
+	public void setContactName(String contactName) {
+		this.contactName = contactName;
+	}
+	public String getContactEmail() {
+		return contactEmail;
+	}
+	public void setContactEmail(String contactEmail) {
+		this.contactEmail = contactEmail;
+	}
+	public Long getContactNumber() {
+		return contactNumber;
+	}
+	public void setContactNumber(Long contactNumber) {
+		this.contactNumber = contactNumber;
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(contactEmail, contactId, contactName, contactNumber);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ContactDTO other = (ContactDTO) obj;
+		return Objects.equals(contactEmail, other.contactEmail) && Objects.equals(contactId, other.contactId)
+				&& Objects.equals(contactName, other.contactName) && Objects.equals(contactNumber, other.contactNumber);
+	}
+	@Override
+	public String toString() {
+		return "ContactDTO [contactId=" + contactId + ", contactName=" + contactName + ", contactEmail=" + contactEmail
+				+ ", contactNumber=" + contactNumber + "]";
+	}
+	
 	
 }
